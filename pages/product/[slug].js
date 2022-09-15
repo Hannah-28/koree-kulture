b17f029ac46a2d35a7fed95b320264fe7ddd04ab
Hannah-28 <omomurewale@gmail.com>
2022-09-15 15:34:21 +0100

    Handling of Add to Cart

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,17 +1,30 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useContext } from 'react';
 import Layout from '../../components/Layout';
 import data from '../../utils/data';
+import { Store } from '../../utils/Store';
 
 export default function ProductScreen() {
+  const { state, dispatch } = useContext(Store);
   const { query } = useRouter();
   const { slug } = query;
   const product = data.products.find((x) => x.slug === slug);
   if (!product) {
     return <div>Product Not Found</div>;
   }
+
+  const addToCartHandler = () => {
+    const existItem = state.cart.cartItems.find((x) => x.slug === product.slug);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+    if (product.countInStock < quantity) {
+      alert('Sorry. Product is out of stock');
+      return;
+    }
+    dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity } });
+  };
+
   return (
     <Layout title={product.name}>
       <div className="py-2">
@@ -26,7 +39,7 @@ export default function ProductScreen() {
             height={640}
             layout="responsive"
           />
-          <div className='flex gap-6 flex-wrap'>
+          <div className="flex gap-6 flex-wrap">
             {product.subImage.map((product, i) => (
               // eslint-disable-next-line @next/next/no-img-element
               <img
@@ -40,7 +53,7 @@ export default function ProductScreen() {
             ))}
           </div>
         </div>
-        <div className='md:col-span-2'>
+        <div className="md:col-span-2">
           <div className="card p-5">
             <div className="mb-2 flex justify-between">
               <div>Price</div>
@@ -50,7 +63,12 @@ export default function ProductScreen() {
               <div>Status</div>
               <div>{product.countInStock > 0 ? 'In Stock' : 'Unavailable'}</div>
             </div>
-            <button className="primary-button w-full">Add to Cart</button>
+            <button
+              className="primary-button w-full"
+              onClick={addToCartHandler}
+            >
+              Add to Cart
+            </button>
           </div>
         </div>
       </div>
